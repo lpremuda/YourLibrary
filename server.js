7d13a01d8ee8f25473c083c1d7129737bfe96d7f
lpremuda <lucas.premuda@gmail.com>
2021-06-03 01:06:58 -0700

    Changed db.once from 'connect' to 'open'

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const db = mongoose.connection
 db.on('error', error => {
     console.error(error)
 })
-db.once('connect', () => {
+db.once('open', () => {
     console.log('Connected to MongoDB database')
 })
 
